3667f1eb58e96fac4c6fe53ee6453b0f093f340c
Drofo <Solodef8@gmail.com>
2024-11-07 15:02:30 +0900

    .

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // implement algorithm here
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Nikita", "First", (byte) 25);
+        userService.saveUser("Mikhail", "Second", (byte) 30);
+        userService.saveUser("Alice", "Third", (byte) 22);
+        userService.saveUser("Chloe", "Fourth", (byte) 28);
+
+        List<User> users = userService.getAllUsers();
+        System.out.println("List of users:");
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // set up a database connection
+    private static final String URL = "jdbc:mysql://localhost:3306/example_schema";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Gun452000";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
