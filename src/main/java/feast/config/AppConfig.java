3fb1ca73d62e3875e15922156eb973e9c3d1bd9e
Drofo <Solodef8@gmail.com>
2024-11-27 16:38:54 +0900

    task_2_1_5

---
+++ src/main/java/feast/config/AppConfig.java
@@ -0,0 +1,16 @@
+package feast.config;
+
+import feast.models.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "feast")
+public class AppConfig {
+
+    @Bean
+    public static Duck2 getDuck(Chicken3 chicken) {
+        return new Duck2(chicken);
+    }
+}
